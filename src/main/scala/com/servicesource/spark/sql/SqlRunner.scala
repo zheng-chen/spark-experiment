134e13540e59c3cb11e966abdaa6540581799bf3
Zheng Chen <zchen@servicesource.com>
2014-09-22 23:26:53 -0700

    refactoring sql runner to support job server

--- src/main/scala/com/servicesource/spark/sql/SqlRunner.scala
+++ src/main/scala/com/servicesource/spark/sql/SqlRunner.scala
@@ -1,63 +1,89 @@
 package com.servicesource.spark.sql
 
-import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.sql._
+import org.apache.spark.sql.SchemaRDD
+import org.apache.spark.rdd.RDD
 import org.apache.hadoop.conf.Configuration
 import org.bson.BSONObject
 import org.bson.BasicBSONObject
 
+import scala.util.Try
+
 import com.servicesource.spark.model._
 import com.servicesource.spark.Settings
 
-object SqlRunner {
+import spark.jobserver._
 
-   def main(args: Array[String]) {
-     val conf = new SparkConf().setAppName("Spark SQL Runner")
-     val sc = new SparkContext(conf)
-     
-//     val sc = new SparkContext("local", "Testing program")
-     val config = new Configuration()
-     
-     val modelObj = Offer
-     
-     config.set("mongo.input.uri", Settings.getDbConnection(modelObj.name))
-     config.set("mongo.input.notimeout", "true");
-     config.set("mongo.splitter.class", "com.servicesource.spark.mongo.CustomCollectionSplitter")
-     
-     if (modelObj.mongoQuery!=null) {
-       config.set("mongo.input.query", modelObj.mongoQuery)
-     }
-     
-     val collRdd = sc.newAPIHadoopRDD(config, classOf[com.mongodb.hadoop.MongoInputFormat], 
-         classOf[Object], classOf[BSONObject])
-         
-     // sql conversion
-     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-     import sqlContext._
-     
-     val mappedColl = collRdd map ( Offer.mapper )
-     mappedColl.cache
-     mappedColl.registerAsTable(modelObj.tableName)
-     
-     for (query <- modelObj.sqlQuery) {
-       sqlContext.sql(query).foreach(Offer.sqlForeachHandler)
-     }
-     
-     
-//     val task = collRdd map ( Asset.mapper )
-//     task.cache
-//     task.registerAsTable("assets")
-//     
-//     val oneTask = sqlContext.sql("SELECT id,disp,amount,customer,product FROM assets WHERE id = \"51a67c8b72338ce8f0007f5d\"")
-//     oneTask.foreach({r=>println("row: "+r)})
-//     
-//     val multiplTasks = sqlContext.sql("SELECT sum(amount), count(id), customer, product FROM assets WHERE disp LIKE \"%Remedy%\" "+
-//         "group by customer, product")
-//     multiplTasks.foreach({r=>println("row: "+r)})
-//     
-//     val multiplTasks1 = sqlContext.sql("SELECT count(id),product FROM assets WHERE disp LIKE \"%Remedy%\" group by product")
-//     multiplTasks1.foreach({r=>println("row: "+r)})
-   }
+import com.typesafe.config.{ Config, ConfigFactory }
+
+object SqlRunner extends SparkJob with NamedRddSupport {
+
+  def main(args: Array[String]) {
+    val sc = new SparkContext("local", "Testing program")
+    loadFromMongo(sc)
+  }
+
+  private def loadFromMongo(sc:SparkContext): RDD[Row] = {
+    val modelObj = Task
+
+    val config = new Configuration()
+    config.set("mongo.input.uri", Settings.getDbConnection(modelObj.name))
+    config.set("mongo.input.notimeout", "true");
+    config.set("mongo.splitter.class", "com.servicesource.spark.mongo.CustomCollectionSplitter")
+
+    if (modelObj.mongoQuery != null) {
+      config.set("mongo.input.query", modelObj.mongoQuery)
+    }
+
+    val collRdd = sc.newAPIHadoopRDD(config, classOf[com.mongodb.hadoop.MongoInputFormat],
+      classOf[Object], classOf[BSONObject])
+
+    val sqlContext: SQLContext = new SQLContext(sc)
+  	import sqlContext._
+  	
+    val mappedColl = collRdd map (Task.mapper)
+    mappedColl.cache
+    mappedColl.registerAsTable(modelObj.tableName)
+    //    for (query <- modelObj.sqlQuery) {
+    //      sqlContext.sql(query).foreach(Task.sqlForeachHandler)
+    //    }
+    mappedColl
+  }
+  
+  private def loadFromS3(sc: SparkContext): (SQLContext, RDD[Row]) = {
+    val modelObj = Offer
+    val collRdd = sc.textFile("s3n://ssi-spark/offers_result_ml/export_sample")
+
+    val sqlContext: SQLContext = new SQLContext(sc)
+  	import sqlContext._
+    val mappedColl = collRdd map (Offer.mapper)
+    mappedColl.cache
+    mappedColl.registerAsTable(modelObj.tableName)
+    (sqlContext, mappedColl)
+    
+  }
+
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
+    Try(config.getString("input.sql"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No input.sql config param"))
+  }
+
+  override def runJob(sc: SparkContext, config: Config): Any = {
+
+//    var rdd = this.namedRdds.get[Row]("cachedRdd").getOrElse(null)
+//
+//    if (rdd == null) {
+//      
+//      this.namedRdds.update("cachedRdd", rdd)
+//    }
+    
+    val (context, rdd) = loadFromS3(sc)
+
+    val sql = config.getString("input.sql")
+    context.sql(sql).collect
+
+  }
 }
